c93f9f798341ba2f5417b46d7721295497a069c3
neardws <neard.ws@gmail.com>
2026-04-08 16:09:17 +0800

    style: pub cards left border default gray, colored on hover by type

--- assets/js/pub-cards.js
+++ assets/js/pub-cards.js
@@ -220,7 +220,7 @@
         ].filter(Boolean).join('');
 
         return `
-          <div class="pub-item ${pub.highlight ? 'highlight' : ''}" data-id="${pub.id}">
+          <div class="pub-item ${pub.highlight ? 'highlight' : ''}" data-id="${pub.id}" data-type="${pub.type}">
             <div class="pub-item-left">
               <span class="pub-type-badge ${pub.type}">${this.getTypeLabel(pub.type)}</span>
               <span class="pub-year-badge">${pub.year}</span>
